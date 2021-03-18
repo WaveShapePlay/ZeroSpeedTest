6169fecb25e6f228e3bf8a59983d53e58ba18d9e
WaveShapePlay <WaveShapePlay@MacBook-Pro.fios-router.home>
2021-03-18 07:15:02 -0400

    Added userInput and built-in analogRead option

--- zeroSpeedTest.ino
+++ zeroSpeedTest.ino
@@ -1,9 +1,13 @@
 #include "Waveforms.h"
 #include "sineWave.h"
 #include "sawTooth.h"
+
+char userInput;
+
 void setup()
 {
-  analogWriteResolution(10); 
+  analogWriteResolution(10);
+  Serial.begin(9600); 
 }
 
 static __inline__ void syncDAC() __attribute__((always_inline, unused));
@@ -18,16 +22,36 @@ void loop(){
   int i = 0;
   int value = 0;
   int wave = 0;
-  while(1){
-    value = sawTooth[i]; // use sinewave for the reference lookup table, or sine for the one created in python
-    syncDAC();
-    DAC->DATA.reg = value & 0x3FF;  // DAC on 10 bits.  // This is a mask - RJ
-    syncDAC();
-    DAC->CTRLA.bit.ENABLE = 0x01;     // Enable DAC // This enables the output - RJ
-    syncDAC();                                  
-    i = i + 1;
-    if(i == 256){
-      i = 0;
-    } // if reset index 
-  } // while true loop
+    if(Serial.available() > 0){
+      userInput = Serial.read();
+      
+      if(userInput == 's'){
+      
+          while(1){
+            value = sinewave[i]; // use sinewave for the reference lookup table, or sine for the one created in python
+            syncDAC();
+            DAC->DATA.reg = value & 0x3FF;  // DAC on 10 bits.  // This is a mask - RJ
+            syncDAC();
+            DAC->CTRLA.bit.ENABLE = 0x01;     // Enable DAC // This enables the output - RJ
+            syncDAC();                                  
+            i = i + 1;
+            if(i == 256){
+              i = 0;
+            } // if reset index 
+          } // while true loop
+          
+      } // if 's' loop
+
+      if(userInput == 't'){
+        while(1){
+          value = sinewave[i];
+          analogWrite(A0,value);
+          
+          i = i + 1;
+          if(i == 256){
+            i = 0;
+          } // if reset index 
+        } // if 't' loop
+      } // while true
+    } // Serial.available
 } // main loop
