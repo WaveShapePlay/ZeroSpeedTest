d59d52dabd3b4db89d7f34de60eb303564b4f724
WaveShapePlay <WaveShapePlay@MacBook-Pro.fios-router.home>
2021-03-20 06:35:27 -0400

    Basic Clean up

--- zeroSpeedTest.ino
+++ zeroSpeedTest.ino
@@ -1,22 +1,18 @@
 #include "Waveforms.h"
-#include "sineWave.h"
-#include "sawTooth.h"
 
 char userInput;
 
 void setup()
 {
-  analogWriteResolution(10);
-  Serial.begin(9600); 
+  analogWriteResolution(10);                // Set DAC output resolution to 10 bits
+  Serial.begin(9600);                       // Initialize serial communication
 }
 
 static __inline__ void syncDAC() __attribute__((always_inline, unused));
 static void syncDAC() {
-  while (DAC->STATUS.bit.SYNCBUSY == 1)
+  while (DAC->STATUS.bit.SYNCBUSY == 1)     // Check DAC Status register 
     ;
 }
-
-// Can create wave.h file
 
 void loop(){
   int i = 0;
@@ -28,30 +24,31 @@ void loop(){
       if(userInput == 's'){
       
           while(1){
-            value = sinewave[i]; // use sinewave for the reference lookup table, or sine for the one created in python
+            value = sinewave[i];            // Use lookup table for sine wave values 
             syncDAC();
-            DAC->DATA.reg = value & 0x3FF;  // DAC on 10 bits.  // This is a mask - RJ
+            DAC->DATA.reg = value & 0x3FF;  // This is a mask for the 10 bit Data value for the DAC
             syncDAC();
-            DAC->CTRLA.bit.ENABLE = 0x01;     // Enable DAC // This enables the output - RJ
-            syncDAC();                                  
+            DAC->CTRLA.bit.ENABLE = 0x01;   // This enables the DAC output
+            syncDAC();
+                                              
             i = i + 1;
-            if(i == 256){
+            if(i == 256){                   // Loop through lookup table & reset index at end
               i = 0;
-            } // if reset index 
-          } // while true loop
+            }                                
+          }                                 
           
-      } // if 's' loop
+      }                                     
 
       if(userInput == 't'){
         while(1){
-          value = sinewave[i];
-          analogWrite(A0,value);
+          value = sinewave[i];              // Use lookup table
+          analogWrite(A0,value);            // Use built in analogwirte function
           
           i = i + 1;
-          if(i == 256){
+          if(i == 256){                     // Loop through lookup table & reset index at end
             i = 0;
-          } // if reset index 
-        } // if 't' loop
-      } // while true
-    } // Serial.available
-} // main loop
+          }
+        } 
+      } 
+    } 
+} 
