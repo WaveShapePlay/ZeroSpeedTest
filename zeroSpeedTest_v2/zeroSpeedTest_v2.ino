dbed8b4e1a5e74cce255ea5f001755959aad94b1
WaveShapePlay <WaveShapePlay@MacBook-Pro.fios-router.home>
2021-05-15 07:55:13 -0400

    Added testing selection options and clean up

--- zeroSpeedTest_v2/zeroSpeedTest_v2.ino
+++ zeroSpeedTest_v2/zeroSpeedTest_v2.ino
@@ -17,17 +17,15 @@ static void syncDAC() {
 char selectSpeedTest(){
   Serial.println("Which speed test would you like to run?");
   char userInputSpeed;
-  Serial.read();
-  Serial.read();
   while(1){
     if(Serial.available() > 0){
       userInputSpeed = Serial.read();
-      if((userInputSpeed == '1')||(userInputSpeed == '2')||(userInputSpeed == '3')||(userInputSpeed == '4') ){
+      if((userInputSpeed == '1')||(userInputSpeed == '2')||(userInputSpeed == '3')||(userInputSpeed == '4')){
         Serial.println(userInputSpeed);
         return userInputSpeed;
       }
       else{
-        Serial.println("Input Error!");
+        Serial.println("Please Enter a value of 1 to 4!");
       }
     }
   }
@@ -42,85 +40,85 @@ void loop(){
      if(userInput == 'b'){
         while(1){
           value = sinewave[i];              // Use lookup table
-          analogWrite(A0,value);            // Use built in analogwirte function
+          analogWrite(A0,value);            
           
           i = i + 1;
           if(i == 256){                     // Loop through lookup table & reset index at end
             i = 0;
           }
         } 
-      } 
+      } // if user selects 'b' - default analogWrite() 
       
       if(userInput == 's'){
         char userSpeedTestType;
-        Serial.println("getting userInput");
+        Serial.println("Getting userInput");
         userSpeedTestType = selectSpeedTest();
-        Serial.println("got speedtest vaule");
+        Serial.println("Got speedtest vaule");
         Serial.println(userSpeedTestType);
         if(userSpeedTestType == '1'){
             while(1){
-            value = sinewave[i];            // Use lookup table for sine wave values 
-            //syncDAC();
-            DAC->DATA.reg = value & 0x3FF;  // This is a mask for the 10 bits of Data value for the DAC
-            syncDAC(); // if all other syncDAC() are commented out, Vout  is slower
-            DAC->CTRLA.bit.ENABLE = 0x01;   // This enables the DAC output
-            syncDAC();
-                                              
-            i = i + 1;
-            if(i == 256){                   // Loop through lookup table & reset index at end
-              i = 0;
-            }
+              value = sinewave[i];            // Use lookup table for sine wave values 
+              syncDAC();
+              DAC->DATA.reg = value & 0x3FF;  // This is a mask for the 10 bits of Data value for the DAC
+              syncDAC();                      
+              DAC->CTRLA.bit.ENABLE = 0x01;   // This enables the DAC output
+              syncDAC();
+                                                
+              i = i + 1;
+              if(i == 256){                   // Loop through lookup table & reset index at end
+                i = 0;
+              }
            } // while True                                
-          } // if user '1'
+          } // if user '1' - using all syncDAC()
 
         if(userSpeedTestType == '2'){
             while(1){
-            value = sinewave[i];            // Use lookup table for sine wave values 
-            //syncDAC();
-            DAC->DATA.reg = value & 0x3FF;  // This is a mask for the 10 bits of Data value for the DAC
-            syncDAC(); // if all other syncDAC() are commented out, Vout  is slower
-            DAC->CTRLA.bit.ENABLE = 0x01;   // This enables the DAC output
-            //syncDAC();
-                                              
-            i = i + 1;
-            if(i == 256){                   // Loop through lookup table & reset index at end
-              i = 0;
-            }
+              value = sinewave[i];            // Use lookup table for sine wave values 
+              syncDAC();
+              DAC->DATA.reg = value & 0x3FF;  // This is a mask for the 10 bits of Data value for the DAC
+              syncDAC(); 
+              DAC->CTRLA.bit.ENABLE = 0x01;   // This enables the DAC output
+              //syncDAC();
+                                                
+              i = i + 1;
+              if(i == 256){                   // Loop through lookup table & reset index at end
+                i = 0;
+              }
            } // while True                                
-          } // if user '2'
+          } // if user '2' - removing last syncDAC()
 
         if(userSpeedTestType == '3'){
             while(1){
-            value = sinewave[i];            // Use lookup table for sine wave values 
-            //syncDAC();
-            DAC->DATA.reg = value & 0x3FF;  // This is a mask for the 10 bits of Data value for the DAC
-            //syncDAC(); // if all other syncDAC() are commented out, Vout  is slower
-            DAC->CTRLA.bit.ENABLE = 0x01;   // This enables the DAC output
-            syncDAC();
-                                              
-            i = i + 1;
-            if(i == 256){                   // Loop through lookup table & reset index at end
-              i = 0;
-            }
+              value = sinewave[i];            // Use lookup table for sine wave values 
+              //syncDAC();
+              DAC->DATA.reg = value & 0x3FF;  // This is a mask for the 10 bits of Data value for the DAC
+              syncDAC(); 
+              DAC->CTRLA.bit.ENABLE = 0x01;   // This enables the DAC output
+              //syncDAC();
+                                                
+              i = i + 1;
+              if(i == 256){                   // Loop through lookup table & reset index at end
+                i = 0;
+              }
            } // while True                                
-          } // if user '3'
-
+          } // if user '3' - synchDAC() after value is write to data register
 
         if(userSpeedTestType == '4'){
             while(1){
-            value = sinewave[i];            // Use lookup table for sine wave values 
-            //syncDAC();
-            DAC->DATA.reg = value & 0x3FF;  // This is a mask for the 10 bits of Data value for the DAC
-            //syncDAC(); // if all other syncDAC() are commented out, Vout  is slower
-            DAC->CTRLA.bit.ENABLE = 0x01;   // This enables the DAC output
-            //syncDAC();
-                                              
-            i = i + 1;
-            if(i == 256){                   // Loop through lookup table & reset index at end
-              i = 0;
-            }
-           } // while True                                
-          } // if user '4'
-        }                                 
-      }                                     
-    } 
+              value = sinewave[i];            // Use lookup table for sine wave values 
+              //syncDAC();
+              DAC->DATA.reg = value & 0x3FF;  // This is a mask for the 10 bits of Data value for the DAC
+              //syncDAC(); 
+              DAC->CTRLA.bit.ENABLE = 0x01;   // This enables the DAC output
+              //syncDAC();
+                                                
+              i = i + 1;
+              if(i == 256){                   // Loop through lookup table & reset index at end
+                i = 0;
+              }
+             } // while True                                
+          } // if user '4' - removing all syncDAC()
+          
+        } // if user selects 's'                                 
+      } // main if Serial.available                                     
+    } // void loop 
